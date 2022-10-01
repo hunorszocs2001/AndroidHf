b3de1d3d53882c59ce086329a49d0614a631900f
hunorszocs2001 <hunorszocs2001@gmail.com>
2022-10-01 15:33:48 +0300

    HF2

--- Homework2.2/app/src/main/java/com/example/activitystatesavetwo/MainActivity.java
+++ Homework2.2/app/src/main/java/com/example/activitystatesavetwo/MainActivity.java
@@ -4,22 +4,40 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    CheckBox checkBox;
+    TextView textbox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        checkBox = findViewById(R.id.checkBox);
+        textbox = findViewById(R.id.textbox);
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
+        outState.putString("editText", textbox.getText().toString());
+        outState.putBoolean("checkBox", checkBox.isChecked());
+
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+        String editText = savedInstanceState.getString("editText");
+        boolean checkboxAllapot = savedInstanceState.getBoolean("checkBox");
+
+        textbox.setText(editText);
+        checkBox.setChecked(checkboxAllapot);
+
     }
 }
